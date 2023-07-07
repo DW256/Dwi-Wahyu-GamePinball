540b96da05df5e0a6317b1a3d4154fccd1313a60
DW256 <dwakurniawan@gmail.com>
2023-07-07 22:52:01 +0700

    Fix Spawner

--- Assets/HideCoin.cs
+++ Assets/HideCoin.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class HideCoin : MonoBehaviour
 {
+    public CoinSpawner spawner;
+    public float lifetime = 10f;
+
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.CompareTag("Ball"))
         {
             // Setelah bola bertabrakan dengan coin, nonaktifkan coin
-            
-            gameObject.SetActive(false);
 
+            //gameObject.SetActive(false);
+            spawner.Despawn(gameObject);
         }
     }
+
+    private void Start()
+    {
+        StartCoroutine(AutoDespawn());
+    }
+
+    private IEnumerator AutoDespawn()
+    {
+        yield return new WaitForSeconds(lifetime);
+        spawner.Despawn(gameObject);
+    }
+
 
 }
--- Assets/Script/CoinSpawner.cs
+++ Assets/Script/CoinSpawner.cs
@@ -4,73 +4,117 @@ using UnityEngine;
 
 public class CoinSpawner : MonoBehaviour
 {
-    public GameObject[] coins;
+    public int maxCoin = 3;
     public float spawnInterval = 3f;
-    public float inactiveDuration = 10f;
     public Transform spawnPoint;
 
     private float duration;
+    public List<GameObject> coins; //pool
+    [SerializeField] private List<GameObject> coinsActive;
 
 
     private void Awake()
     {
-        for (int i = 0; i < coins.Length; i++)
+        //Make sure all coins are deactivated
+        for (int i = 0; i < coins.Count; i++)
         {
             coins[i].SetActive(false);
         }
     }
 
-
-    private void Update()
+    public void Despawn(GameObject obj)
     {
-       duration = inactiveDuration - Time.deltaTime;
+        coinsActive.Remove(obj);
+        obj.SetActive(false);
+        coins.Add(obj);
 
-        if(duration <= 0)
-        {
-            inactiveDuration = 10f;
-
-            
-        }
-
-        StartCoroutine(SpawnCoins());
+        //respawn
+        StartCoroutine(Respawn());
     }
 
-    private IEnumerator SpawnCoins()
+    private void Spawn()
     {
-        while (true)
+        if (coinsActive.Count >= maxCoin) return;
+
+        //Random coin dari list
+        int rand = Random.Range(0, coins.Count);
+
+        coins[rand].SetActive(true);
+        coinsActive.Add(coins[rand]);
+        coins.Remove(coins[rand]);
+
+    }
+
+    private void Start()
+    {
+        StartCoroutine(StartSpawn());
+    }
+
+    private IEnumerator StartSpawn()
+    {
+        for (int i = 0; i < maxCoin; i++)
         {
-            // Cek jika masih ada coin yang aktif
-            int activeCoinCount = 0;
-            foreach (GameObject coin in coins)
-            {
-                if (coin.activeSelf)
-                    activeCoinCount++;
-            }
-
-            // Spawn coin jika jumlah aktif kurang dari maksimal
-            if (activeCoinCount < 3)
-            {
-                // Random coin dari array
-                int randomIndex = Random.Range(0, coins.Length);
-                GameObject coinToSpawn = coins[randomIndex];
-
-                if(coinToSpawn.activeInHierarchy == false)
-                {
-                    coinToSpawn.SetActive(true);
-                }
-
-                if (duration <= 0)
-                {
-                    coinToSpawn.SetActive(false);
-                    activeCoinCount--;
-                }
-                
-            }
-
-            // Jeda sebelum melakukan spawn kembali
             yield return new WaitForSeconds(spawnInterval);
+            Spawn();
         }
     }
+
+    private IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(spawnInterval);
+        Spawn();
+    }
+
+    //private void Update()
+    //{
+    //   duration = inactiveDuration - Time.deltaTime;
+
+    //    if(duration <= 0)
+    //    {
+    //        inactiveDuration = 10f;
+
+
+    //    }
+
+    //    StartCoroutine(SpawnCoins());
+    //}
+
+    //private IEnumerator SpawnCoins()
+    //{
+    //    while (true)
+    //    {
+    //        // Cek jika masih ada coin yang aktif
+    //        int activeCoinCount = 0;
+    //        foreach (GameObject coin in coins)
+    //        {
+    //            if (coin.activeSelf)
+    //                activeCoinCount++;
+    //        }
+
+    //        // Spawn coin jika jumlah aktif kurang dari maksimal
+    //        if (activeCoinCount < 3)
+    //        {
+    //            // Random coin dari array
+    //            int randomIndex = Random.Range(0, coins.Count);
+    //            GameObject coinToSpawn = coins[randomIndex];
+
+    //            if(coinToSpawn.activeInHierarchy == false)
+    //            {
+    //                coinToSpawn.SetActive(true);
+    //            }
+
+    //            if (duration <= 0)
+    //            {
+    //                coinToSpawn.SetActive(false);
+    //                activeCoinCount--;
+    //            }
+
+    //        }
+
+    //        // Jeda sebelum melakukan spawn kembali
+    //        yield return new WaitForSeconds(spawnInterval);
+    //    }
+    //}
 
 
 
